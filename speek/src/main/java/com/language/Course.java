183c4d6d458780468d8cd0ea29af95f49a13da79
graciedd <graciedd@email.sc.edu>
2024-10-27 17:04:49 -0400

    updating javaDoc Course

--- speek/src/main/java/com/language/Course.java
+++ speek/src/main/java/com/language/Course.java
@@ -19,6 +19,9 @@ public class Course {
     private String category;
     private HashMap<String, Story> stories;
 
+    /**
+     * Course constructor
+     */
     public Course(UUID courseID, String course, HashMap<String, ArrayList<Phrase>> phrases,  HashMap<String, ArrayList<Word>>words, String category, HashMap<String, Story> stories ){
         this.courseID = courseID;
         this.course = course;
@@ -28,68 +31,137 @@ public class Course {
         this.stories = stories;
     }
 
-    
+    /**
+     * getCourseID method
+     * gets courseID
+     * @return courseID
+     */
     public UUID getCourseID(){
         return courseID;
     }
 
+    /**
+    * setCourseID method
+    * sets courseID
+    */
     public void setCourseID(UUID courseID){
         this.courseID = courseID;
     }
 
+    /**
+     * getCourse method
+     * gets course
+     * @return course
+     */
     public String getCourse(){
         return course;
     }
 
+    /**
+    * setCourse method
+    * sets course
+    */
     public void setCourse(String course){
         this.course = course;
     }
 
+    /**
+     * getPhrases method
+     * gets phrases list
+     * @return phrases
+     */
     public HashMap<String, ArrayList<Phrase>> getPhrases(){
         return phrases;
     }
 
+    /**
+     * getPhrasesByCategory
+     * gets phrases based on category
+     * @return phrases.getOrDefault(category, new ArrayList<>())
+     */
     public ArrayList<Phrase> getPhrasesByCategory(String category) {
         return phrases.getOrDefault(category, new ArrayList<>());
     }
 
+    /**
+     * getCategory method
+     * gets category
+     * @return category
+     */
     public String getCategory(){
         return category;
     }
 
+    /**
+     * getWords method
+     * gets list of words
+     * @return words
+     */
     public HashMap<String, ArrayList<Word>> getWords(){
         return words;
     }
 
+     /**
+     * getWordsByCategory method
+     * gets words based on category
+     * @return words.getOrDefault(category2, new ArrayList<>())
+     */
     public ArrayList<Word> getWordsByCategory(String category2) {
         return words.getOrDefault(category2, new ArrayList<>());
     }
 
+    /**
+     * getStories method
+     * gets list of stories
+     * @return stories
+     */
     public HashMap<String, Story> getStories() {
         return stories;
     }
 
+    /**
+    * getStoriesByCategory method
+    * gets stories based on category
+    * @return stories.get(category)
+    */
     public Story getStoriesByCategory(String category) {
         return stories.get(category);
     }
     
+    /**
+    * setCategory method
+    * sets category
+    */
     public void setCategory(String category){
         this.category = category;
     }
 
-     public ArrayList<String> getAvailableCourse() {
+    /**
+     * getAvailableCourses method
+     * gets available courses
+     * @return courses
+     */
+    public ArrayList<String> getAvailableCourse() {
         ArrayList<String> courses = new ArrayList<>();
         courses.add("Phrases");
         courses.add("Words");
         return courses;
     }
 
+     /**
+    * chooseCourse method
+    * chooses course from list
+    */
     public String chooseCourse(ArrayList<String> courseNames){
         String selectedCourse = "Phrases";
         return selectedCourse;
     }
 
-     public String toString() {
+     /**
+    * toString method
+    * prints out phrases and words in courses
+    */
+    public String toString() {
         String result = "";
 
         result += "title: " + course + "\n";
@@ -113,5 +185,5 @@ public class Course {
             }
 
         return result;
-     }
+    }
 }
