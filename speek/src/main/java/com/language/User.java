f93fa536f1bdd7ecb00f48631e5b713613dc2764
graciedd <graciedd@email.sc.edu>
2024-10-27 18:56:37 -0400

    updating javaDoc for user

--- speek/src/main/java/com/language/User.java
+++ speek/src/main/java/com/language/User.java
@@ -9,9 +9,7 @@ import java.util.UUID;
  */
 
 public class User{
-/**
- * attributes for user class
- */
+
     private UUID id;
     private String firstName;
     private String lastName;
@@ -22,6 +20,17 @@ public class User{
     private String password;
     private HashMap<Course,Progress> courses;
 
+    /**
+     * Constructor for User that makes a HashMap
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param phoneNumber
+     * @param dateOfBirth
+     * @param username
+     * @param password
+     */
     public User(UUID id, String firstName, String lastName, String email, String phoneNumber, LocalDate dateOfBirth, String username, String password){
         this.id = id;
         this.firstName = firstName;
@@ -34,70 +43,162 @@ public class User{
         this.courses = new HashMap <>();
     }
 
+    /**
+     * Constructor for User
+     * @param id
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param phoneNumber
+     * @param dateOfBirth
+     * @param username
+     * @param password
+     * @param courses
+     */
     public User(UUID id, String firstName, String lastName, String email, String phoneNumber, LocalDate dateOfBirth, String username, String password, HashMap<Course,Progress> courses) {
         this(id, firstName, lastName, email, "", dateOfBirth, username, password);
     }
 
+    /**
+     * getID method
+     * Returns user ID
+     * @return id
+     */
     public UUID getId() {
         return id;
     }
 
+    /**
+     * getFirstName method
+     * Returns user's first name
+     * @return firstName
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * setFirstName method
+     * Sets user's first name
+     * @param firstName
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+     /**
+     * getLastName method
+     * Returns user's last name
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * setLastName method
+     * Sets user's last name
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * getEmail method
+     * Returns user's email
+     * @return email
+     */
     public String getEmail(){
         return email;
     }
 
+    /**
+     * setEmail method
+     * Sets user's email
+     * @param email
+     */
     public void setEmail(String email){
         this.email = email;
     }
 
+    /**
+     * getUsername method
+     * Returns user's username
+     * @return username
+     */
     public String getUsername(){
         return username;
     }
 
+    /**
+     * setUsername method
+     * Sets user's username
+     * @param username
+     */
     public void setUsername(String username){
         this.username = username;
     }
 
+    /**
+     * getEnterUsername method
+     * Returns user's entered username
+     * @return username
+     */
     public String getEnterUsername(){
         return username;
     }
 
+    /**
+     * setEnterUsername method
+     * Sets user's entered username
+     * @param username
+     */
     public void setEnterUsername(String username){
         this.username = username;
     }
 
+    /**
+     * getPassword method
+     * Returns user's password
+     * @return password
+     */
     public String getPassword(){
         return password;
     }
 
+    /**
+     * setPassword method
+     * Sets user's password
+     * @param password
+     */
     public void setPassword(String password){
         this.password = password;
     }
 
+    /**
+     * getEnterPassword method
+     * Returns user's entered password
+     * @return password
+     */
     public String getEnterPassword() {
         return password;
     }
 
+    /**
+     * setEnterPassword method
+     * Sets user's entered password
+     * @param password
+     */
     public void setEnterPassword(String username){
         this.username = username;
     }
 
+    /**
+     * getPhoneNumber method
+     * Returns user's phone numer if it is in a valid format
+     * @return phoneNumber
+     */
     public String getPhoneNumber(){
         if (phoneNumber != null && phoneNumber.length() == 10) {
             return phoneNumber.substring(0, 3) + "-" + phoneNumber.substring(3, 6) + "-" + phoneNumber.substring(6);
@@ -105,31 +206,66 @@ public class User{
         return phoneNumber;
     }
 
+    /**
+     * setPhoneNumber method
+     * Sets user's phone number
+     * @param phoneNumber
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * getDateOfBirth method
+     * Returns user's dateOfBirth
+     * @return dateOfBirth
+     */
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
+     /**
+     * setDateOfBirth method
+     * Sets user's dateOfBirth
+     * @param dateOfBirth
+     */
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * getFormattedDateOfBirth method
+     * Returns user's dateOfBirth formatted in pattern MM/dd/yyyy
+     * @return dateOfBirth
+     */
     public String getFormattedDateOfBirth() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         return dateOfBirth.format(formatter);
     }
 
+     /**
+     * getCourses method
+     * Returns HashMap of courses and progress
+     * @return courses
+     */
     public HashMap<Course, Progress> getCourses(){
         return courses;
     }
 
-    public void setLanguages(HashMap<Course, Progress> courses) {
+    /**
+     * setCourses method
+     * Sets courses HashMap
+     * @param courses
+     */
+    public void setCourses(HashMap<Course, Progress> courses) {
         this.courses = courses;
     }
 
+     /**
+     * forgotPassword method
+     * Sets password to default if the user is not valid
+     * @param newPassword
+     */
     public void forgotPassword(String newPassword){
         String defaultPassword = "LanguageLearner123@";
         if (newPassword.length() <= 8 || newPassword.length() > 20 || !newPassword.matches(".*[!@#$%^&*()].*")) {
@@ -140,6 +276,12 @@ public class User{
         }
     }
 
+    /** 
+    * toString method
+    * Returns User attributes in toString format
+    * @return result
+    */
+
     public String toString() {
         String result = "";
         result += id + "--" + firstName + "--" + lastName +"--" + email +"--" + phoneNumber + 
@@ -147,7 +289,7 @@ public class User{
 
         return result; 
     
-}
+    }
 }
 
 
