39df47cd418a6f00fb810ad43a3daa7dbc56be62
Zaniah Kirkpatrick <zaniahk@email.sc.edu>
2024-10-30 18:56:14 -0400

    test

--- speek/src/main/java/com/language/UITest.java
+++ speek/src/main/java/com/language/UITest.java
@@ -1,6 +1,7 @@
 package com.language;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class UITest {
     private CategorySystemFacade facade;
@@ -15,6 +16,7 @@ public class UITest {
         createAccount();
         login();
         chooseLangauage();
+        chooseCategory();
         logout();
 
     }
@@ -56,23 +58,30 @@ public class UITest {
         facade.chooseLanguage("Spanish");
     }
 
-    public void chooseCourse() 
-    {
-        Course course = facade.getCourse(); 
-        if (course != null) 
-        { 
-            System.out.println("User is now learing" + Course.getCourse()); 
+    // public void chooseCourse() 
+    // {
+    //     Course course = facade.getCourse(); 
+    //     if (course != null) 
+    //     { 
+    //         System.out.println("User is now learing" + Course.getCourse()); 
+    //     }
+    // }
+
+    public void chooseCategory(){
+        if(facade.getCategory().isEmpty()){
+            System.out.println("Couldnt find category");
         }
+        System.out.println("youre learning " + Category.NUMBERS);
     }
 
     
     // boolean isCorrectAnswer = true;
-    public void progressLearning() {
-        facade.manageProgress(isCorrectAnswer);
-        System.out.print("Tracking the progress of what the user has learned.");
+    // public void progressLearning() {
+    //     facade.manageProgress(isCorrectAnswer);
+    //     System.out.print("Tracking the progress of what the user has learned.");
 
-        int totalQuestionsAnswered = 5;
-        int numCorrectAnswers = 4;
+    //     int totalQuestionsAnswered = 5;
+    //     int numCorrectAnswers = 4;
         
     // }
 
