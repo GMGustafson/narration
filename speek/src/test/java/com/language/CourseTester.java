cee620d202548fd452b42513bb184aeea60e86cb
graciedd <graciedd@email.sc.edu>
2024-11-03 19:24:29 -0500

    uploading facade

---
+++ speek/src/test/java/com/language/CourseTester.java
@@ -0,0 +1,7 @@
+package com.language;
+
+public class CourseTester {
+    
+    
+
+}
--- speek/src/test/java/com/language/DataWriterTester.java
+++ speek/src/test/java/com/language/DataWriterTester.java
@@ -1,30 +1,37 @@
 package com.language;
 
+
 // import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 
 import java.util.ArrayList;
 import java.util.List;
 import java.time.LocalDate;
 import java.util.HashMap;
 
+
 // import org.junit.jupiter.api.BeforeEach;
 // import org.junit.jupiter.api.AfterEach;
 //import org.junit.jupiter.api.Test;
+
 
 import com.language.DataLoader;
 import com.language.DataWriter;
 import com.language.User;
 import com.language.UserList;
 
+
 public class DataWriterTester {
     private UserList users = UserList.getInstance();
     private ArrayList<User> userList = users.getUsers();
+
 
     @Before
     public void setUp() {
@@ -32,17 +39,20 @@ public class DataWriterTester {
         DataWriter.saveUsers();
     }
 
+
     @After
     public void tearDown() {
         UserList.getInstance().getUsers().clear();
         DataWriter.saveUsers();
     }
 
+
     @Test
     void testWritingNoUsers() {
         userList = DataLoader.getUsers();
         assertEquals(0, userList.size());
     }
+
 
     @Test
     void testWritingOneUser() {
@@ -51,31 +61,26 @@ public class DataWriterTester {
         assertEquals("jdoe", DataLoader.getUsers().get(0).getUsername());
    }
 
+
    @Test
    void testWritingUserProgress() {
     User user = new User(null, "John", "Doe", "jdoe@gmail.com", "627-917-2739",null, "2004-12-8", "ilovemyCat");
     Progress progress = new Progress(10, 7, "colors", 5, 2, new ArrayList<>(List.of("verde", "amarillo")));
     user.setProgress(progress);
     userList.add(user);
-    
+   
     DataWriter.saveUsers();
-    
+   
     Progress loadedProgress = DataLoader.getUsers().get(0).getProgress();
     assertEquals(7, loadedProgress.getNumCorrectAnswers());
     assertEquals("colors", loadedProgress.getCurrentCategory());
    }
 
-   @Test
-   void testWritingCourseWithCategories() {
-    Course course = new Course("Spanish", "8c24fea9-e192-457d-8ba3-db5828f62490");
-    Category category = new Category("numbers");
-    course.addCategory(category);
-    
-    DataWriter.saveCourse(course);
-    
-    HashMap<Course,Progress> loadedCourse = DataLoader.getCourses().get(0);
-    assertEquals("numbers", loadedCourse.getCategories().get(0).getTitle());
-}
+
+
+
+
+
 
 
 @Test
@@ -85,6 +90,7 @@ void testWritingEmptyUser() {
     assertEquals("", DataLoader.getUsers().get(0).getUsername());
 }
 
+
 @Test
 void testWritingNullUser() {
     userList.add(new User(null, null, null, null, null, null, null, null));
@@ -92,14 +98,16 @@ void testWritingNullUser() {
     assertNull(DataLoader.getUsers().get(0).getUsername());
 }
 
+
 @Test
 void testMultipleUsers() {
     userList.add(new User(null, "John", "Doe", "jdoe@gmail.com", "803-144-1021", null, "jdoe", "ilovemycat"));
     userList.add(new User(null, "Amy", "Smith", "asmith@gamil.com","803-456-9037", null, "jdoe", "ilovemycat"));
-    
+   
     DataWriter.saveUsers();
-    
+   
     assertEquals("asmith", DataLoader.getUsers().get(1).getUsername());
 }
 }
+
 
